2cc07f5a9c9646945a3fc2365ff8dd024687db8b
Surrenity <surren@surrenity.com>
2013-09-07 22:16:51 -0500

    Chap 6 exercises

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -14,6 +14,7 @@ describe User do
 
   it { should be_valid }
 
+##name validation
   describe "when name is not present" do
     before { @user.name = " " }
     it { should_not be_valid }
@@ -28,10 +29,11 @@ describe User do
     it { should_not be_valid }
   end
 
+##email validation
   describe "when email format is invalid" do
     it "should be invalid" do
       addresses = %w[user@foo,com user_at_foo.org example.user@foo.
-                     foo@bar_baz.com foo@bar+baz.com]
+                     foo@bar_baz.com foo@bar+baz.com foo@bar..com]
       addresses.each do |invalid_address|
         @user.email = invalid_address
         expect(@user).not_to be_valid
@@ -59,6 +61,17 @@ describe User do
     it { should_not be_valid }
   end
 
+  describe "email address with mixed case" do
+    let(:mixed_case_email) { "Foo@ExAMPle.CoM" }
+
+    it "should be saved as all lower-case" do
+      @user.email = mixed_case_email
+      @user.save
+      expect(@user.reload.email).to eq mixed_case_email.downcase
+    end
+  end
+
+##password validation
   describe "when password is not present" do
     before do
       @user = User.new(name: "Example User", email: "user@example.com",
